0a4520ce5c66685d8dc7bba39952d0a1a884521f
Gabriel Bach <gabriel@inter-action.design>
2021-03-02 17:42:42 +0100

    adjust file directory for slick scripts

--- inc/classes/class-sm-assets.php
+++ inc/classes/class-sm-assets.php
@@ -18,10 +18,10 @@ class SalemocheAssets {
         wp_register_script( 'main', SALEMOCHE_ESSENTIALS_JS_URL . '/main.js', [ 'jquery' ], false, false );
         wp_enqueue_script( 'main' );
 
-        wp_register_style( 'slick-css', UZHxUNIGE_BUILD_CSS_URI . '/slick.css', [], filemtime( UZHxUNIGE_BUILD_CSS_DIR_PATH . '/slick.css'), 'all' );
+        wp_register_style( 'slick-css', SALEMOCHE_ESSENTIALS_CSS_URL . '/slick.css', [], false, 'all' );
         wp_enqueue_style('slick-css');
         
-        wp_register_script( 'slick-js', UZHxUNIGE_BUILD_JS_URI . '/slick.js', [ 'jquery' ], filemtime( UZHxUNIGE_BUILD_JS_DIR_PATH . '/slick.js'), true );
+        wp_register_script( 'slick-js', SALEMOCHE_ESSENTIALS_JS_URL . '/slick.js', [ 'jquery' ], false, true );
         wp_enqueue_script('slick-js');
 
     }
